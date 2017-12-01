aa0c2dc35b4382556bb9d559f73ee55473aaa52e
Zimo Chai (Jerry) <zic337@utulsa.edu>
2017-12-01 22:53:20 +0000

    Create a bash script for statistic usage

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -f p2
+
+gcc -o p2 p2.c
+
+./p2 chr1.fa > statistic.txt
+
+echo -n "=============================="
+echo -n " head statistic.txt "
+echo "=============================="
+
+head statistic.txt
+
+echo -n "=============================="
+echo -n " tail statistic.txt "
+echo "=============================="
+
+tail -n 15 statistic.txt
