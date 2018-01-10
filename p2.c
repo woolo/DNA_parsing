8401364ad3fff6a756d3bdbe4cdb39f7ae94b3f2
Zimo Chai (Jerry) <zic337@utulsa.edu>
2018-01-10 01:06:07 +0000

    Fix bugs and simplify logic
    1. Fix bug which mishandled the newline character
    2. Simplify the logic
    3. Reduce hardcoding

--- p2.c
+++ p2.c
@@ -35,19 +35,41 @@ word 1    location 1, location 2, .. , location n
 word 2    location 1, location 2, .. , location n
 
 Note: word 1, word 2, and word n are unique.
+
+Desgin:
+============================
+Since the data we are going to process is huge, we need to think about the tradeoff between computation resource and the RAM we have.
+In this implementation, we go through the entire file twice:
+For the first time, we need to count the occurence times of each word, which helps us to allocate only RAM that we will use;
+For the second time, we store the location to the porper place of the array.
+Therefore, although we process the entire file twice, which cause a little bit more computation resource, it saves us tons of RAM comparing to allocating a SIZE*SIZE 2d array.
 */
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
+/*
+  Notice that changing WORDSIZE here is not enough when wordsize really changed.
+  This is becuase, still, 0xFFFFF is hardcoded.
+*/
+#define WORDSIZE 10
+/*
+  This number is caculated by (NUMBER_OF_VALID_CHARACTER)^WORDSIZE = 4 ^ 10
+*/
 #define SIZE 1048576
 
-int a[SIZE];                    /* intitialize an array with 2^20 0 */
+int a[SIZE];                    /* intitialize an array with 4^10 0 */
+int *a_2d[SIZE];
 
 int main(int argc, char **argv){
   char c;
   FILE *f;
+  unsigned int index;
+  unsigned int value;
+  int length = 0;   /* length is current length of a complete/incomplete word */
+  int location = -WORDSIZE + 1;    /* therefore the location of the first word will be 1 */
 
   if(argc == 2){
     f = fopen(argv[1], "r"); /* "r" for read */
@@ -57,13 +79,61 @@ int main(int argc, char **argv){
     return 1;
   }
 
-  unsigned int index;
-  unsigned int value;
+  while((c = getc(f)) != EOF ){
+    bool isValidValue = true;
+    switch(c){
+    case 'A':
+    case 'a':
+      value = 0x0;
+      break;
+    case 'T':
+    case 't':
+      value = 0x1;
+      break;
+    case 'C':
+    case 'c':
+      value = 0x2;
+      break;
+    case 'G':
+    case 'g':
+      value = 0x3;
+      break;
+    case '\n':
+      continue;
+    default:
+      isValidValue = false;
+      length = 0;       /* important to get consecutive 10 characters by reseting length to 0 */
+    }
+    if(isValidValue){
+      ++length;
+      /* shift left the current index by 2 bits*/
+      index = index << 2;
+      /* "add" value to the index by bitwise OR */
+      index = index | value;
+      /* only record the valid words (10 consecutive characters) */
+      if(length >= WORDSIZE){
+        ++a[index & 0xFFFFF];
+      }
+    }
+  }    // end of while loop
 
-  int location = 0;
 
-  // now read the file
-  while ((c = getc(f)) != EOF){
+  /*
+    Create the 2d array based on the statistic data of occurence of each pattern.
+    We intentionally allocate one more cell (a[i] +1) for the array because we will use
+    a_2d[index][0] as a counter for how many elements have been filled into the arrat.
+   */
+  for (int i=0; i<SIZE; i++){
+    a_2d[i] = (int *)malloc( (a[i]+1) * sizeof(int));
+  }
+
+  /* now read the file again
+     this time, we will remember the location of the file
+   */
+  fseek(f, 0, SEEK_SET);     /* reset the pointer to the beginning of the file */
+  length = 0;                /* reset the length to 0 since this will a new loop of counting */
+  while((c = getc(f)) != EOF ){
+    ++location;
     bool isValidValue = true;
     switch(c){
     case 'A':
@@ -84,78 +154,36 @@ int main(int argc, char **argv){
       break;
     case '\n':
       --location;
-      isValidValue = false;
-      break;
+      continue;
     default:
       isValidValue = false;
+      length = 0;       /* important to get 10 consecutive characters by reseting length to 0 */
     }
     if(isValidValue){
+      ++length;
       /* shift left the current index by 2 bits*/
       index = index << 2;
       /* "add" value to the index by bitwise OR */
       index = index | value;
       /* Use the lower 20 bits as a hash */
-      ++a[index & 0xFFFFF];
-      ++location;
-    }
-    else{
-      /* use a for loop to get a valid word (10 consecutive characters) */
-      int length = 0;
-      while(((c = getc(f)) != EOF ) && length <= 10){
-        ++location;
-        bool isValidValue = true;
-        switch(c){
-        case 'A':
-        case 'a':
-          value = 0x0;
-          break;
-        case 'T':
-        case 't':
-          value = 0x1;
-          break;
-        case 'C':
-        case 'c':
-          value = 0x2;
-          break;
-        case 'G':
-        case 'g':
-          value = 0x3;
-          break;
-        case '\n':
-          isValidValue = false;
-          --location;
-          break;
-        default:
-          isValidValue = false;
-          length = 0;       /* important to get consecutive 10 characters by reseting length to 0 */
-        }
-        if(isValidValue){
-          ++length;
-          /* shift left the current index by 2 bits*/
-          index = index << 2;
-          /* "add" value to the index by bitwise OR */
-          index = index | value;
-        }
-      }    // end of for loop
-      /* after the for loop we got the valid address */
-      ++a[index & 0xFFFFF];
-    }  // end of else
-  }  // end of while loop
+      /* We store the current location to the proper place in the 2nd array.
+         Note that we also increase the a_2d[index & 0xFFFFF][0] which indicates
+         the number of elements in the 2nd array by 1.
+       */
+      if(length >= WORDSIZE){
+        a_2d[index & 0xFFFFF][++a_2d[index & 0xFFFFF][0]] = location;
+      }
+    } /* end if statement */
+  }   /* end while loop */
 
-  int max = 0;
-  int max_of_index = 0;
-  int above400 = 0;
-  int above500 = 0;
-  int above600 = 0;
-  int above700 = 0;
-  int above800 = 0;
-  int above900 = 0;
-
+  // print out the result
+  // k is the row index in this case
   for(int k = 0; k < SIZE; k++){
     int i = k;
     /* the for loop is for printing the word pattern */
-    for(int j=0; j < 10; j++){
-      switch( (i & 0xC0000) >> 18){  // get the value in of 20th and 19th bit as a 2 bit binary
+    for(int j=0; j < WORDSIZE; j++){
+      /* (i & 0xC0000) >> 18 */
+      switch((i >>(WORDSIZE - 1)*2) & 0x3){ // get the value in of 20th and 19th bit as a 2 bit binary
       case 0x0:
         printf("%c", 'A');
         break;
@@ -171,50 +199,20 @@ int main(int argc, char **argv){
       default:
         fprintf(stderr, "Error: Unexpected pasred value.\n");
         return 1;
-      }
+      }  // end switch
       i = i << 2;
-    }
-    printf(" %d\n", a[k]);
+    }  // end inter for loop
 
-    max_of_index = a[k] > max ? k : max_of_index;
-    max = a[k] > max ? a[k] : max;
-    above400 += a[k] > 400 ? 1 : 0;
-    above500 += a[k] > 500 ? 1 : 0;
-    above600 += a[k] > 600 ? 1 : 0;
-    above700 += a[k] > 700 ? 1 : 0;
-    above800 += a[k] > 800 ? 1 : 0;
-    above900 += a[k] > 900 ? 1 : 0;
-  }
-
-  int i = max_of_index;
-  for(int j=0; j < 10; j++){
-      switch( (i  & 0xC0000) >> 18){  // get the value in of 20th and 19th bit as a 2 bit binary
-      case 0x0:
-        printf("%c", 'A');
-        break;
-      case 0x1:
-        printf("%c", 'T');
-        break;
-      case 0x2:
-        printf("%c", 'C');
-        break;
-      case 0x3:
-        printf("%c", 'G');
-        break;
-      default:
-        fprintf(stderr, "Error: Unexpected pasred value.\n");
-        return 1;
-      }
-      i = i << 2;
+    printf(":    ");     /* format */
+    // Note that a_2d[row][col] is same as *(*(arr+row)+col)
+    /* we start the index of the 2nd array from 1 because a_2d[k][0] hold the value of total number of elements in that array
+       of course, we can print that out for debugging reason.
+     */
+    for (int col = 1; col < a_2d[k][0]+1; col++){
+        printf(" %d,", a_2d[k][col]);
     }
-  printf(" has the most cocurrences: %d\n", max);
-  printf("The occurrence of numbers above 400 is: %d\n", above400);
-  printf("The occurrence of numbers above 500 is: %d\n", above500);
-  printf("The occurrence of numbers above 600 is: %d\n", above600);
-  printf("The occurrence of numbers above 700 is: %d\n", above700);
-  printf("The occurrence of numbers above 800 is: %d\n", above800);
-  printf("The occurrence of numbers above 900 is: %d\n", above900);
-  printf("Final location value: %d\n", location);
+    printf("\n");
+  }  // end outer for loop
 
   return 0;
 }
